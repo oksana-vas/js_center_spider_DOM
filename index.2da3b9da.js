6c96f167664f6d075645a42900ae5466bce7cf67
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-29 13:55:44 +0100

    make build

---
+++ index.2da3b9da.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientHeight,l=e.clientWidth,i=t.clientHeight,n=t.clientWidth;t.style.top="".concat(c/2-i/2,"px"),t.style.left="".concat(l/2-n/2,"px");
+//# sourceMappingURL=index.2da3b9da.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallHeight = wall.clientHeight;
+const wallWidth = wall.clientWidth;
+const spiderHeight = spider.clientHeight;
+const spiderWidth = spider.clientWidth;
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
